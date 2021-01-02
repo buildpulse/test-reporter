14cdeba1541faac28a50b7247c465a66bc991e8f
Jason Rudolph <jason@jasonrudolph.com>
2021-01-02 08:57:07 -0500

    Test with distinct time zones for committed/authored timestamps

--- metadata/commit_test.go
+++ metadata/commit_test.go
@@ -25,19 +25,18 @@ func Test_repositoryCommitResolver_Lookup(t *testing.T) {
 	r, err := NewCommitResolver(dir)
 	require.NoError(t, err)
 
-	c, err := r.Lookup("616a8b20906bd3820fb489148ed69516be3b98ad")
+	c, err := r.Lookup("5974e4edce87279f60adaf55c2adcee8847b2612")
 	require.NoError(t, err)
 
-	assert.Equal(t, "Thu Dec 31 01:02:03 -0500 2020", c.AuthoredAt.Format(time.UnixDate))
+	assert.Equal(t, "Thu Dec 31 01:02:03 +1300 2020", c.AuthoredAt.Format(time.UnixDate))
 	assert.Equal(t, "dwight@dundermifflin.com", c.AuthorEmail)
 	assert.Equal(t, "Dwight Kurt Schrute III", c.AuthorName)
-	assert.Equal(t, "Fri Jan  1 13:14:15 -0500 2021", c.CommittedAt.Format(time.UnixDate))
+	assert.Equal(t, "Fri Jan  1 13:14:15 -0800 2021", c.CommittedAt.Format(time.UnixDate))
 	assert.Equal(t, "jim@dundermifflin.com", c.CommitterEmail)
 	assert.Equal(t, "Jim Halpert", c.CommitterName)
 	assert.Equal(t, "Add some flair\n", c.Message)
-	assert.Equal(t, "616a8b20906bd3820fb489148ed69516be3b98ad", c.SHA)
+	assert.Equal(t, "5974e4edce87279f60adaf55c2adcee8847b2612", c.SHA)
 	assert.Equal(t, "eb8b39c87131c1f3543bc6e5a426f7d4d631bc15", c.TreeSHA)
-
 }
 
 func Test_repositoryCommitResolver_Lookup_notFound(t *testing.T) {
